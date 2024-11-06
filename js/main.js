1f251bdd11be2d4a4a71683b4467ed60ed62e794
Sam Mangan <sam.mangan2@gmail.com>
2024-11-06 12:00:47 +1100

    dark mode only

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() { 
   const themeButton = document.querySelector("button#darkmodetoggle");
   themeButton.addEventListener("click", () => document.body.classList.toggle("darkmode"));
-
-  document.querySelectorAll(".card").forEach(card => {
-    card.addEventListener("click", () => card.classList.toggle("flipped"));
-  })
 })
