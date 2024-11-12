e2fa386397acc4105947ebca40a17fe69ae2b446
Sam Mangan <sam.mangan2@gmail.com>
2024-11-13 07:13:41 +1100

    Exercises 1-5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() { 
-  const themeButton = document.querySelector("button#darkmodetoggle");
-  themeButton.addEventListener("click", () => document.body.classList.toggle("darkmode"));
-})
+// JavaScript uses double-forward-slash for single-line comments
+
+/* You can also write block comments like this:
+everything between the slash-star and the star-slash is a comment */
+
+
+// Unlike Python, variables can be declared without being assigned a value
+let themeButton; // Declare themeButton, but don't assign
+console.log(themeButton===undefined); // true
+
+// Select the darkmode button, and assign the resulting Element object to themeButton
+themeButton = document.querySelector("button#darkmodetoggle");
+console.log(themeButton===undefined); // false
+
+// Declare a function called toggleDarkMode with no paramaters (a.k.a. arguments)
+function toggleDarkMode() {
+  let body = document.querySelector("body"); // Declare AND assign body
+  body.classList.toggle("darkmode"); // Add or remove the 'darkmode' class
+}
+
+// When the button is clicked, call toggleDarkMode
+themeButton.addEventListener("click", toggleDarkMode); 
---
+++ js/week5.js
@@ -0,0 +1,39 @@
+/* Exercise 0 */
+let body = document.querySelector("body");
+body.classList.add("darkmode");
+
+/* Exercise 1 */
+let h1 = document.querySelector("h1");
+h1.style.fontFamily = "Dancing Script";
+h1.style.color = "seagreen";
+
+/* Exercise 2 */
+h1.textContent = "The " + h1.textContent;
+
+/* Exercise 3 */
+function toggleFancyHeading() {
+  if (h1.style.color === "seagreen") {
+    h1.style.color = "var(--text)";
+    h1.style.fontFamily = "Montserrat";
+  } else {
+    h1.style.color = "seagreen";
+    h1.style.fontFamily = "Dancing Script";
+  }
+}
+h1.addEventListener("click", toggleFancyHeading);
+
+/* Exercise 4 */
+let logo = document.querySelector("#heading img");
+function spinLogo() {
+  let startFrame = { transform: "rotate(0deg)" };
+  let endFrame = { transform: "rotate(360deg)" };
+  logo.animate([startFrame, endFrame], 200);
+}
+logo.addEventListener("dblclick", spinLogo);
+
+/* Exercise 5 */
+let plantHeadings = document.querySelectorAll(".card .front h2");
+function makeFancy(heading) {
+  heading.style.fontFamily = "Dancing Script";
+}
+plantHeadings.forEach(makeFancy);
