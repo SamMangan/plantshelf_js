79db4146958b6d8750b1945297b63f03230b281c
Sam <110870725+SamMangan@users.noreply.github.com>
2024-11-14 13:34:09 +1100

    Update week5.js

--- js/week5.js
+++ js/week5.js
@@ -1,40 +1,13 @@
 console.log("Welcome to Week 5!");
 
-/* Exercise 0 */
-// console.log("This week we meet the browser dev tools!");
+/* Exercise 1: Log the message "This week we meet the browser dev tools!" */
 
-/* Exercise 1 */
-// let h1 = document.querySelector("h1");
-// h1.style.fontFamily = "Dancing Script";
-// h1.style.color = "seagreen";
+/* Exercise 2: Set the main heading to be green and have font "Dancing Script" */
 
-/* Exercise 2 */
-// h1.textContent = "The " + h1.textContent;
+/* Exercise 3: Set the main heading to be The Plant Shelf */
 
-/* Exercise 3 */
-// function toggleFancyHeading() {
-//   if (h1.style.color === "seagreen") {
-//     h1.style.color = "var(--text)";
-//     h1.style.fontFamily = "Montserrat";
-//   } else {
-//     h1.style.color = "seagreen";
-//     h1.style.fontFamily = "Dancing Script";
-//   }
-// }
-// h1.addEventListener("click", toggleFancyHeading);
+/* Exercise 4: Make the main heading toggle between the original style and the green/Dancing Script style when clicked */
 
-/* Exercise 4 */
-let logo = document.querySelector("#heading img");
-function spinLogo() {
-  let startFrame = { transform: "rotate(0deg)" };
-  let endFrame = { transform: "rotate(360deg)" };
-  logo.animate([startFrame, endFrame], 200);
-}
-logo.addEventListener("click", spinLogo);
+/* Exercise 5: */
 
-/* Exercise 5 */
-let plantHeadings = document.querySelectorAll(".card .front h2");
-function makeFancy(heading) {
-  heading.style.fontFamily = "Dancing Script";
-}
-// plantHeadings.forEach(makeFancy);
+/* Exercise 6: */
